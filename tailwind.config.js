7cc684912a472b22db6131a6217457918db5bfe8
Tim Traylor <ttraylor83@gmail.com>
2023-03-01 17:54:45 +0000

    cleanup and fixes for meta tags

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        deepBlue: '#041f60'
+      },
+      
+    },
   },
   plugins: [],
 }
