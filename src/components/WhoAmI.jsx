d860e20bc017efc30f61661373744e0aba6da869
Tim Traylor <ttraylor83@gmail.com>
2023-03-07 15:50:29 +0000

    updated history framework

---
+++ src/components/WhoAmI.jsx
@@ -0,0 +1,9 @@
+const WhoAmI = () => {
+  return (
+    <>
+      <h1>Tim Traylor</h1>
+      <h2>Full Stack JavaScript Developer</h2>
+    </>
+  )
+}
+export default WhoAmI
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,13 @@
 import PastProjects from '../components/PastProjects'
 import Skills from '../components/Skills'
 import Skills2 from '../components/Skills2'
+import WhoAmI from '../components/WhoAmI'
 
 
 const Home = () => {
   return (
     <div>
+      <WhoAmI />
       {/* <Skills /> */}
       <Skills2 />
       <PastProjects />
