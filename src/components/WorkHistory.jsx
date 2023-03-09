12b21b5457970c5cb28df520dc23c80a138ceb72
Tim Traylor <ttraylor83@gmail.com>
2023-03-09 04:08:43 +0000

    different color testing

--- src/components/WorkHistory.jsx
+++ src/components/WorkHistory.jsx
@@ -1,5 +1,6 @@
-import {IoSchoolSharp} from 'react-icons/io5'
-import {GiElectric} from 'react-icons/gi'
+import { IoSchoolSharp } from 'react-icons/io5'
+import { GiElectric } from 'react-icons/gi'
+import { MdAnchor } from 'react-icons/md'
 import {
   VerticalTimeline,
   VerticalTimelineElement,
@@ -11,123 +12,86 @@ const WorkHistory = () => {
     <>
       <VerticalTimeline layout='2-columns' lineColor='#2D61DD'>
 
-      <VerticalTimelineElement
+        <VerticalTimelineElement
           // className='vertical-timeline-element--work'
           position={'right'}
-          contentStyle={{background: '#AEDEAD', color: '#fff'}}
-          contentArrowStyle={{ borderRight: '7px solid  #AEDEAD' }}
+          contentStyle={{ background: '#fff', color: '#000000' }}
+          contentArrowStyle={{ borderRight: '7px solid  #fff' }}
           date='June 2022 - November 2022'
           iconStyle={{ background: '#AEDEAD', color: '#fff' }}
           icon={<IoSchoolSharp />}
         >
-          <h3 className='vertical-timeline-element-title'>Software Developer</h3>
-          <h4 className='vertical-timeline-element-subtitle'>
-            Seattle, WA
-          </h4>
+          <h3 className='vertical-timeline-element-title text-gray-600'>
+            Software Developer
+          </h3>
+          <h4 className='vertical-timeline-element-subtitle'>Seattle, WA</h4>
           <p>Advanced Software Development in Full-Stack JavaScript</p>
         </VerticalTimelineElement>
 
         <VerticalTimelineElement
           // className='vertical-timeline-element--work'
+          position={'right'}
           contentStyle={{ background: '#2196F3', color: '#fff' }}
           contentArrowStyle={{ borderRight: '7px solid  #2196F3' }}
-          date='2011 - present'
+          date='June 2017 - December 2021'
           iconStyle={{ background: '#2196F3', color: '#fff' }}
           icon={<GiElectric />}
         >
-          <h3 className='vertical-timeline-element-title'>Lead Industrial Maintenance Instrumentation</h3>
+          <h3 className='vertical-timeline-element-title'>
+            Lead Industrial Maintenance Instrumentation
+          </h3>
           <h3>& Electrical Technician</h3>
           <h4 className='vertical-timeline-element-subtitle'>Seattle, WA</h4>
           <ul>
-            <li>Supervised 12 expert technicians in day-to-day maintenance and prioritization of critical plant repairs, complex project development between plant staff and multiple contractors to facilitate direction, development, and deconfliction.</li>
-            <li>Provided technical analysis input to a wide variety of renovation and new construction projects ranging from minor upgrades of software to fundamental system design retrofit with budgets totaling over $100 million.</li>
+            <li>
+              Supervised 12 expert technicians in day-to-day maintenance and
+              prioritization of critical plant repairs, complex project
+              development between plant staff and multiple contractors to
+              facilitate direction, development, and deconfliction.
+            </li>
+            <li>
+              Provided technical analysis input to a wide variety of renovation
+              and new construction projects ranging from minor upgrades of
+              software to fundamental system design retrofit with budgets
+              totaling over $100 million.
+            </li>
           </ul>
         </VerticalTimelineElement>
 
-
-        {/* <VerticalTimelineElement
-          className='vertical-timeline-element--work'
-          date='2010 - 2011'
-          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-          icon={<WorkIcon />}
+        <VerticalTimelineElement
+          // className='vertical-timeline-element--work'
+          position='left'
+          contentStyle={{ background: '#2196F3', color: '#fff' }}
+          contentArrowStyle={{ borderRight: '7px solid  #2196F3' }}
+          date='November 2004 - April 2017'
+          iconStyle={{ background: '#2196F3', color: '#fff' }}
+          icon={<MdAnchor />}
         >
-          <h3 className='vertical-timeline-element-title'>Art Director</h3>
-          <h4 className='vertical-timeline-element-subtitle'>
-            San Francisco, CA
-          </h4>
+          <h3 className='vertical-timeline-element-title' >
+            Electrical Division Leading Chief Petty Officer
+          </h3>
+          <h4 className='vertical-timeline-element-subtitle'>Silverdale, WA</h4>
           <p>
-            Creative Direction, User Experience, Visual Design, SEO, Online
-            Marketing
+            <ul>
+              <li>
+                Trained, managed, planned, and organized 15 diverse personnel in
+                over 40,000 yearly man/hours of maintenance and repair of
+                complex systems that support submarine and nuclear power plant
+                operations.
+              </li>
+              <li>
+                Interfaced and collaborated with 13 cross-functional teams and
+                external contractors to formulate and plan work flow, time
+                management, logistics, and resource management
+              </li>
+              <li>
+                Master Training Specialist: Instructed junior technicians on
+                electrical theory, maintenance, and reactor plant systems with a
+                99.4% pass rate of over 400 students and 5 instructors
+              </li>
+            </ul>
           </p>
-        </VerticalTimelineElement> */}
-
-
-        {/* <VerticalTimelineElement
-          className='vertical-timeline-element--work'
-          date='2008 - 2010'
-          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-          icon={<WorkIcon />}
-        >
-          <h3 className='vertical-timeline-element-title'>Web Designer</h3>
-          <h4 className='vertical-timeline-element-subtitle'>
-            Los Angeles, CA
-          </h4>
-          <p>User Experience, Visual Design</p>
-        </VerticalTimelineElement> */}
-
-
-
-
-
-        {/* <VerticalTimelineElement
-          className='vertical-timeline-element--education'
-          date='April 2013'
-          iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-          icon={<SchoolIcon />}
-        >
-          <h3 className='vertical-timeline-element-title'>
-            Content Marketing for Web, Mobile and Social Media
-          </h3>
-          <h4 className='vertical-timeline-element-subtitle'>Online Course</h4>
-          <p>Strategy, Social Media</p>
-        </VerticalTimelineElement> */}
-
-
-        {/* <VerticalTimelineElement
-          className='vertical-timeline-element--education'
-          date='November 2012'
-          iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-          icon={<SchoolIcon />}
-        >
-          <h3 className='vertical-timeline-element-title'>
-            Agile Development Scrum Master
-          </h3>
-          <h4 className='vertical-timeline-element-subtitle'>Certification</h4>
-          <p>Creative Direction, User Experience, Visual Design</p>
-        </VerticalTimelineElement> */}
-
-
-        {/* <VerticalTimelineElement
-          className='vertical-timeline-element--education'
-          date='2002 - 2006'
-          iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-          icon={<SchoolIcon />}
-        >
-          <h3 className='vertical-timeline-element-title'>
-            Bachelor of Science in Interactive Digital Media Visual Imaging
-          </h3>
-          <h4 className='vertical-timeline-element-subtitle'>
-            Bachelor Degree
-          </h4>
-          <p>Creative Direction, Visual Design</p>
-        </VerticalTimelineElement> */}
-
-
-        {/* <VerticalTimelineElement
-          iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
-          icon={<StarIcon />}
-        /> */}
-
+        </VerticalTimelineElement>
 
       </VerticalTimeline>
     </>
