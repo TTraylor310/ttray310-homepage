59eb16da19f260f4dfffb928704269a812af8cf7
Tim Traylor <ttraylor83@gmail.com>
2023-03-23 01:01:04 +0000

    bio copied

--- src/components/WhoAmI.jsx
+++ src/components/WhoAmI.jsx
@@ -3,6 +3,7 @@ const WhoAmI = () => {
     <>
       <h1>Tim Traylor</h1>
       <h2>Full Stack JavaScript Developer</h2>
+      <p>Full-stack Javascript software developer from diverse background of Naval Nuclear Power and electrical troubleshooting supervisor. I have a passion for complex problem solving with team and goals focused results. Fine tuning my skills in JavaScript via React, projects have been extremely rewarding and I look forward to future projects.</p>
     </>
   )
 }
